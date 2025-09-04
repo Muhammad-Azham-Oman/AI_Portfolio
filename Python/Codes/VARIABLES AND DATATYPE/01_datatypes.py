50eae160491c95bb9b1986f487ca48152d0ed2f5
Muhammad-Azham-Oman <azhamoman10@gmail.com>
2025-09-04 11:37:54 +0500

    Delete Python/Codes/VARIABLES AND DATATYPE directory

--- Python/Codes/VARIABLES AND DATATYPE/01_datatypes.py
+++
@@ -1,9 +0,0 @@
-a = 2 # this is an intger
-
-b = 5.86 # this is a floating point number
-
-c = "Honey" #this is a string
-
-d = True # this is a boolean
-
-e = None # this is a none type vriable
--- Python/Codes/VARIABLES AND DATATYPE/02_variables.py
+++
@@ -1,6 +0,0 @@
-a = 5  # a is variable
-b = 5  # b is variable
-print(a-b)
-
-name = "Honey" # name is variable
-print(name)
