bb3bf3e4f1cab6aba956ffe286cdf5c8e9301cb0
Muhammad-Azham-Oman <azhamoman10@gmail.com>
2025-09-04 11:30:11 +0500

    Add files via upload

---
+++ Python/Codes/VARIABLES AND DATATYPE/03_rules_variables.py
@@ -0,0 +1,15 @@
+a = 32
+
+bgfs = 67
+
+Honey = 89
+
+# Gr@ny = 76 ## Invalid due to @
+
+_Azham = 96
+
+H_oney = 56.8
+
+# @Honey = 76 ## Invalid due to @
+
+# Hello World = 76 ## Invalid due to space
---
+++ Python/Codes/VARIABLES AND DATATYPE/05_type.py
@@ -0,0 +1,24 @@
+a = 56
+b = type(a) # Type of a is integer
+print(b)
+
+c = 34.65
+d = type(c) # Type of c is float
+print(d)
+
+e = "Honey"
+f = type(e) # Type of e is string
+print(f)
+
+g = "34"
+h = type(g) # Type of g is string
+print(h)
+
+i = "87.6"
+j = type(i) # Type of i is string
+print(j)
+
+k = "76.4"
+l = float(k)
+m = type(l) # Type of c is float
+print(m)
